e506e37f0075b1e91f2b6699863539c36a4a0f3b
Leon <leon@famnabuurs.nl>
2021-04-12 14:48:25 +0200

    Opdracht afgerond

---
+++ js/applicatie.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc Academy");
+
+let naam = "Leon";
+console.log(naam);
+
+//let uitkomst = 4 + 4;
+//console.log(uitkomst);
+let uitkomst = "4" + "4";
+console.log(uitkomst);
+
+let age = 61;
+console.log (typeof(age));
